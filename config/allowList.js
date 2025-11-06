df6627fd98405a8a61c04bff4086f6b10fbeb204
Henry <Henries90@gmail.com>
2025-11-06 23:17:58 +0100

    Eliminated trailing slash

--- config/allowList.js
+++ config/allowList.js
@@ -1,7 +1,7 @@
 const allowList = [
   "http:/10.249.49.156:3000",
   "http://localhost:3000",
-  "https://admission-compass-frontend.vercel.app/",
+  "https://admission-compass-frontend.vercel.app",
 ];
 
 module.exports = allowList;
