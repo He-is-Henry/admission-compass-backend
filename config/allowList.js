cbcdddc3301bd1423e53a9fba92a28a04aa08905
Henry <Henries90@gmail.com>
2025-11-06 23:16:59 +0100

    Added a new url to allowlits

--- config/allowList.js
+++ config/allowList.js
@@ -1,3 +1,7 @@
-const allowList = ["http:/10.249.49.156:3000", "http://localhost:3000"];
+const allowList = [
+  "http:/10.249.49.156:3000",
+  "http://localhost:3000",
+  "https://admission-compass-frontend.vercel.app/",
+];
 
 module.exports = allowList;
